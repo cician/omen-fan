4e2ae070ffcb09c6b450e7f22b5d788bb8271d49
Maciej Kacper Jagiełło <2879867+cician@users.noreply.github.com>
2025-12-24 23:38:53 +0100

    nixpkgs 25.11 instead of unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Python Template";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
